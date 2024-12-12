8115523f7eca13ba1a43d4a4b818b67f879e760a
franciscovaz <ftomasvaz@gmail.com>
2024-12-12 23:16:19 +0000

    - add header component; - add logo;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.module.css'
+import { Header } from './components/Header'
 import './global.css'
 
 function App() {
   return (
     <>
+      <Header />
       <p>Todo List</p>
     </>
   )
