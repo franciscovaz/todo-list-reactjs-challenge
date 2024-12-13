dda492683071dd304f275c50e7901151e643312b
franciscovaz <ftomasvaz@gmail.com>
2024-12-13 23:41:11 +0000

    - add components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import './App.module.css'
 import { Header } from './components/Header'
 import { NewTask } from './components/NewTask'
+import { Tasks } from './components/Tasks'
+import { TasksHeader } from './components/TasksHeader'
+import styles from './App.module.css';
 import './global.css'
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
-      
-      <NewTask />
-    </>
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <NewTask />
+        </div>
+
+        <div className={styles.tasksList}>
+            <TasksHeader/>
+
+            <Tasks />
+        </div>
+      </section>
+    </main>
   )
 }
 
